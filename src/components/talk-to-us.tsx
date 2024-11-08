6a8f3136f99619750cee1f2900cee7ccfc477ff3
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2024-11-08 17:26:09 -0300

    feat: adjust talk to us section

--- src/components/talk-to-us.tsx
+++ src/components/talk-to-us.tsx
@@ -9,50 +9,60 @@ export function TalkToUs() {
     <section id="talk-to-us" className="w-full py-24">
       <div className="mx-auto flex h-full max-w-[1120px] flex-col items-center justify-center">
         <h2 className="mb-12 text-4xl text-white">Fale conosco!</h2>
-        <h3 className="mb-4 text-center text-3xl text-white">
+        <h3 className="mb-10 text-center text-3xl text-white">
           Quer receber uma{' '}
           <b className="text-purple-400">consultoria gratuita</b> com nosso
           estrategista?
         </h3>
-        <h4 className="mb-28 text-center text-xl text-white">
-          Preencha o formulário com suas informações, e entraremos em contato
-          para marcar o melhor horário.
-        </h4>
-        <div className="flex w-full items-center gap-2">
-          <div className="w-full">
-            <Label htmlFor="name" className="text-white">
-              Nome
-            </Label>
-            <Input id="name" placeholder="Nome" />
+        <div className="flex w-full items-start gap-8">
+          <div className="w-1/2 space-y-3">
+            <h4 className="text-xl text-white">
+              Preencha o formulário com suas informações, e entraremos em
+              contato para marcar o melhor horário.
+            </h4>
+            <div className="w-full">
+              <Label htmlFor="name" className="text-white">
+                Nome
+              </Label>
+              <Input id="name" placeholder="Nome" />
+            </div>
+            <div className="w-full">
+              <Label htmlFor="email" className="text-white">
+                E-mail
+              </Label>
+              <Input className="w-full" id="email" type="email" />
+            </div>
+            <div className="w-full">
+              <Label htmlFor="phone" className="text-white">
+                Telefone
+              </Label>
+              <Input id="phone" />
+            </div>
+            <div className="w-full">
+              <Label htmlFor="company" className="text-white">
+                Nome da sua empresa:
+              </Label>
+              <Input id="company" />
+            </div>
+            <div className="flex w-full flex-col items-center justify-end gap-2">
+              <Button className="w-full bg-purple-600 px-8 py-4 text-lg hover:bg-purple-700">
+                Quero uma consultoria
+              </Button>
+            </div>
           </div>
-          <div className="w-full">
-            <Label htmlFor="email" className="text-white">
-              E-mail
-            </Label>
-            <Input className="w-full" id="email" type="email" />
+          <div className="w-1/2 space-y-3 text-white">
+            <div className="min-h-14">
+              <h4 className="text-xl">
+                Ou fale conosco diretamente pelos nossos contatos:
+              </h4>
+            </div>
+            <p className="mb-4">
+              <b>WhatsApp:</b> 23432423
+            </p>
+            <p>
+              <b>Telefone:</b> 2343242342
+            </p>
           </div>
-        </div>
-        <div className="mt-4 flex w-full items-center gap-2">
-          <div className="w-full">
-            <Label htmlFor="phone" className="text-white">
-              Telefone
-            </Label>
-            <Input id="phone" />
-          </div>
-          <div className="w-full">
-            <Label htmlFor="company" className="text-white">
-              Nome da sua empresa:
-            </Label>
-            <Input id="company" />
-          </div>
-        </div>
-        <div className="flex w-full flex-col items-center justify-end gap-2">
-          <Button className="mt-8 w-56 bg-purple-600 px-8 py-4 text-lg hover:bg-purple-700">
-            Quero uma consultoria
-          </Button>
-          <Button className="text-green-400" variant="link">
-            Ou fale diretamente conosco pelo whatsapp
-          </Button>
         </div>
       </div>
     </section>
